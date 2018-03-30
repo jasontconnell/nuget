ee2c16106466f01550dd9c54d438fd61b0344cc6
Jason Connell <jconnell@delphicdigital.com>
2018-03-30 11:53:21 -0400

    Better higher version selection... still not great.

--- nuget.go
+++ nuget.go
@@ -1,296 +1,298 @@
 package nuget
 
 import (
-    "fmt"
-    "net/http"
-    "encoding/json"
-    "io"
-    "path"
-    "path/filepath"
-    "os"
-    "archive/zip"
-    "strings"
-    "strconv"
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-func decodeJson(r io.Reader, obj interface{}){
-    dec := json.NewDecoder(r)
-    dec.Decode(obj)
+func decodeJson(r io.Reader, obj interface{}) {
+	dec := json.NewDecoder(r)
+	dec.Decode(obj)
 }
 
 type NugetService struct {
-    DefinitionUrl string
-    resources ResourceList
+	DefinitionUrl string
+	resources     ResourceList
 }
 
-
 func NewService(url string) *NugetService {
-    n := new(NugetService)
-    n.DefinitionUrl = url
-    return n
+	n := new(NugetService)
+	n.DefinitionUrl = url
+	return n
 }
 
 func Download(svcUrl, id, version, folder string) (string, error) {
-    var err error
-    svc := NewService(svcUrl)
-    err = svc.GetResources()
+	var err error
+	svc := NewService(svcUrl)
+	err = svc.GetResources()
 
-    if err != nil {
-        return "", fmt.Errorf("Couldn't get resources, %v", err)
-    }
+	if err != nil {
+		return "", fmt.Errorf("Couldn't get resources, %v", err)
+	}
 
-    pkgdata,err := svc.GetPackageData(id)
+	pkgdata, err := svc.GetPackageData(id)
 
-    if err != nil {
-        return "", fmt.Errorf("Couldn't get package data, %v", err)
-    }
+	if err != nil {
+		return "", fmt.Errorf("Couldn't get package data, %v", err)
+	}
 
-    v := svc.GetVersion(pkgdata, version)
+	v := svc.GetVersion(pkgdata, version)
 
-    if v.Version == "" {
-        return "", fmt.Errorf("Version not found, %v", version)
-    }
+	if v.Version == "" {
+		return "", fmt.Errorf("Version not found, %v", version)
+	}
 
-    fn, err := svc.DownloadVersion(v, folder)
+	fn, err := svc.DownloadVersion(v, folder)
 
-    if err != nil {
-        return "", fmt.Errorf("Error downloading version, %v.", err)
-    }
+	if err != nil {
+		return "", fmt.Errorf("Error downloading version, %v.", err)
+	}
 
-    return fn, nil
+	return fn, nil
 }
 
 func DownloadAndExtract(svcUrl, id, version, downloadFolder, extractFolder string) (string, error) {
-    fn, err := Download(svcUrl, id, version, downloadFolder)
+	fn, err := Download(svcUrl, id, version, downloadFolder)
 
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    zr,zrerr := zip.OpenReader(filepath.Join(downloadFolder, fn))
+	zr, zrerr := zip.OpenReader(filepath.Join(downloadFolder, fn))
 
-    if zrerr != nil {
-        return "", zrerr
-    }
+	if zrerr != nil {
+		return "", zrerr
+	}
 
-    defer zr.Close()
+	defer zr.Close()
 
-    dir := filepath.Join(extractFolder, id, version)
-    merr := os.MkdirAll(dir, os.ModePerm)
+	dir := filepath.Join(extractFolder, id, version)
+	merr := os.MkdirAll(dir, os.ModePerm)
 
-    if merr != nil {
-        return "", merr
-    }
+	if merr != nil {
+		return "", merr
+	}
 
-    for _, f := range zr.File {
-        fpath,n := path.Split(f.Name)
-        extPath := filepath.Join(append([]string{dir}, strings.Split(fpath, "/")...)...)
-        meerr := os.MkdirAll(extPath, os.ModePerm)
-        if meerr != nil {
-            fmt.Println("couldn't create folder", extPath)
-            continue
-        }
-        fout,ferr := os.Create(filepath.Join(extPath, n))
-        docopy := true
+	for _, f := range zr.File {
+		fpath, n := path.Split(f.Name)
+		extPath := filepath.Join(append([]string{dir}, strings.Split(fpath, "/")...)...)
+		meerr := os.MkdirAll(extPath, os.ModePerm)
+		if meerr != nil {
+			fmt.Println("couldn't create folder", extPath)
+			continue
+		}
+		fout, ferr := os.Create(filepath.Join(extPath, n))
+		docopy := true
 
-        if ferr != nil {
-            fmt.Println(ferr)
-            docopy = false
-        }
+		if ferr != nil {
+			fmt.Println(ferr)
+			docopy = false
+		}
 
-        rc, rcerr := f.Open()
-        if rcerr != nil {
-            fmt.Println(rcerr)
-            docopy = false
-        }
+		rc, rcerr := f.Open()
+		if rcerr != nil {
+			fmt.Println(rcerr)
+			docopy = false
+		}
 
-        if docopy {
-            _, cerr := io.Copy(fout, rc)
-            if cerr != nil {
-                fmt.Println(cerr)
-            }
-        }
+		if docopy {
+			_, cerr := io.Copy(fout, rc)
+			if cerr != nil {
+				fmt.Println(cerr)
+			}
+		}
 
-        rc.Close()
-    }
+		rc.Close()
+	}
 
-    return dir, nil
+	return dir, nil
 }
 
 func GetLatestVersion(svcUrl, id string) (string, error) {
-    var err error
-    svc := NewService(svcUrl)
-    err = svc.GetResources()
+	var err error
+	svc := NewService(svcUrl)
+	err = svc.GetResources()
 
-    if err != nil {
-        return "", fmt.Errorf("Couldn't get resources, %v", err)
-    }
+	if err != nil {
+		return "", fmt.Errorf("Couldn't get resources, %v", err)
+	}
 
-    pkgdata,err := svc.GetPackageData(id)
-    version := getHighestVersion(pkgdata.Versions)
+	pkgdata, err := svc.GetPackageData(id)
+	version := getHighestVersion(pkgdata.Versions)
 
-    return version, nil
+	return version, nil
 }
 
 func getHighestVersion(versions []Version) string {
-    vs := ""
-    high := int64(0)
-    for _, v := range versions {
-        pts := strings.Split(v.Version, ".")
-        m := int64(1)
-        cv := int64(0)
-        for i := len(pts) - 1; i >= 0; i-- {
-            pt := pts[i]
-            if len(pt) > 2 {
-                pt = string(pt[:2])
-            }
-            parsed, _ := strconv.ParseInt(pt, 10, 64)
-            cv = cv + (parsed * m)
-            m = m * int64(100)
-        }
+	vs := ""
+	high := int64(0)
+	for _, v := range versions {
+		pts := strings.Split(v.Version, ".")
+		if len(pts) > 3 {
+			pts = pts[:3]
+		}
+		m := int64(1)
+		cv := int64(0)
+		for i := len(pts) - 1; i >= 0; i-- {
+			pt := pts[i]
+			if len(pt) > 2 {
+				pt = string(pt[:2])
+			}
+			parsed, _ := strconv.ParseInt(pt, 10, 64)
+			cv = cv + (parsed * m)
+			m = m * int64(100)
+		}
 
-        if cv > high {
-            high = cv
-            vs = v.Version
-        }
-    }
-    return vs
+		if cv > high {
+			high = cv
+			vs = v.Version
+		}
+	}
+
+	return vs
 }
 
 func (svc *NugetService) GetResources() error {
-    if len(svc.resources.Resources) > 0 {
-        return nil
-    }
+	if len(svc.resources.Resources) > 0 {
+		return nil
+	}
 
-    resp, err := http.Get(svc.DefinitionUrl)
-    if err != nil {
-        return fmt.Errorf("Couldn't get resources, %v", err)
-    }
+	resp, err := http.Get(svc.DefinitionUrl)
+	if err != nil {
+		return fmt.Errorf("Couldn't get resources, %v", err)
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    decodeJson(resp.Body, &svc.resources)
+	decodeJson(resp.Body, &svc.resources)
 
-    if len(svc.resources.Resources) == 0 {
-        return fmt.Errorf("Couldn't get resources, no resources in body")
-    }
+	if len(svc.resources.Resources) == 0 {
+		return fmt.Errorf("Couldn't get resources, no resources in body")
+	}
 
-    return nil
+	return nil
 }
 
 func (svc *NugetService) getQueryService() (Resource, error) {
-    r := Resource{}
-    err := svc.GetResources()
-    if err != nil {
-        return r, fmt.Errorf("Couldn't get resources, %v", err)
-    }
+	r := Resource{}
+	err := svc.GetResources()
+	if err != nil {
+		return r, fmt.Errorf("Couldn't get resources, %v", err)
+	}
 
-    // just get the first query service
-    for _, res := range svc.resources.Resources {
-        if res.Type == "SearchQueryService" {
-            r = res
-            break
-        }
-    }
+	// just get the first query service
+	for _, res := range svc.resources.Resources {
+		if res.Type == "SearchQueryService" {
+			r = res
+			break
+		}
+	}
 
-    if r.Id == "" {
-        return r, fmt.Errorf("Couldn't find query service, %v", svc.DefinitionUrl)
-    }
+	if r.Id == "" {
+		return r, fmt.Errorf("Couldn't find query service, %v", svc.DefinitionUrl)
+	}
 
-    return r, nil
+	return r, nil
 }
 
 func (svc *NugetService) GetPackageData(id string) (ResultData, error) {
-    pkg := ResultData{}
+	pkg := ResultData{}
 
-    q, err := svc.getQueryService()
+	q, err := svc.getQueryService()
 
-    if err != nil {
-        return pkg, err
-    }
+	if err != nil {
+		return pkg, err
+	}
 
-    url := fmt.Sprintf(`%s?q=@Id:"%s"&prerelease=false`, q.Id, id)
-    resp, rerr := http.Get(url)
-    if rerr != nil {
-        return pkg, fmt.Errorf("Failed in call to query, %v", rerr)
-    }
+	url := fmt.Sprintf(`%s?q=@Id:"%s"&prerelease=false`, q.Id, id)
+	resp, rerr := http.Get(url)
+	if rerr != nil {
+		return pkg, fmt.Errorf("Failed in call to query, %v", rerr)
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    qr := QueryResult{}
-    decodeJson(resp.Body, &qr)
+	qr := QueryResult{}
+	decodeJson(resp.Body, &qr)
 
-    if qr.TotalHits == 0 {
-        return pkg, fmt.Errorf("No results, %v", url)
-    }
+	if qr.TotalHits == 0 {
+		return pkg, fmt.Errorf("No results, %v", url)
+	}
 
-    for _, res := range qr.Data {
-        if res.Id == id {
-            pkg = res
-            break
-        }
-    }
+	for _, res := range qr.Data {
+		if res.Id == id {
+			pkg = res
+			break
+		}
+	}
 
-    return pkg, nil
+	return pkg, nil
 }
 
 func (svc *NugetService) GetVersion(res ResultData, version string) Version {
-    v := Version{}
-    for _, ver := range res.Versions {
-        if ver.Version == version {
-            v = ver
-            break
-        }
-    }
+	v := Version{}
+	for _, ver := range res.Versions {
+		if ver.Version == version {
+			v = ver
+			break
+		}
+	}
 
-    return v
+	return v
 }
 
 func (svc *NugetService) DownloadVersion(version Version, folder string) (string, error) {
 
-    if filepath.IsAbs(folder){
-        err := os.MkdirAll(folder, os.ModePerm)
-        if err != nil {
-            return "", fmt.Errorf("Couldn't create directories, %v - %v.", folder, err)
-        }
-    }
+	if filepath.IsAbs(folder) {
+		err := os.MkdirAll(folder, os.ModePerm)
+		if err != nil {
+			return "", fmt.Errorf("Couldn't create directories, %v - %v.", folder, err)
+		}
+	}
 
-    vd,vderr := svc.getVersionData(version)
-    if vderr != nil {
-        return "", vderr
-    }
-    _, fn := path.Split(vd.PackageUrl)
-    f,err := os.Create(filepath.Join(folder, fn))
+	vd, vderr := svc.getVersionData(version)
+	if vderr != nil {
+		return "", vderr
+	}
+	_, fn := path.Split(vd.PackageUrl)
+	f, err := os.Create(filepath.Join(folder, fn))
 
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    resp, gerr := http.Get(vd.PackageUrl)
-    if gerr != nil {
-        return "", gerr
-    }
+	resp, gerr := http.Get(vd.PackageUrl)
+	if gerr != nil {
+		return "", gerr
+	}
 
-    defer resp.Body.Close()
-    defer f.Close()
+	defer resp.Body.Close()
+	defer f.Close()
 
-    
-    _, cerr := io.Copy(f, resp.Body)
+	_, cerr := io.Copy(f, resp.Body)
 
-    return fn, cerr
+	return fn, cerr
 }
 
 func (svc *NugetService) getVersionData(version Version) (VersionData, error) {
-    vd := VersionData{}
-    resp, err := http.Get(version.Url)
+	vd := VersionData{}
+	resp, err := http.Get(version.Url)
 
-    if err != nil {
-        return vd, err
-    }
+	if err != nil {
+		return vd, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    decodeJson(resp.Body, &vd)
+	decodeJson(resp.Body, &vd)
 
-    return vd, nil
+	return vd, nil
 }
--- nuget_test.go
+++ nuget_test.go
@@ -1,152 +1,153 @@
 package nuget
 
 import (
-    "testing"
+	"testing"
 )
 
 var serviceUrl = "https://api.nuget.org/v3/index.json"
 
 func GetService(t *testing.T) *NugetService {
-    t.Helper()
+	t.Helper()
 
-    n := NewService(serviceUrl)
-    return n
+	n := NewService(serviceUrl)
+	return n
 }
 
-func TestCreate(t *testing.T){
-    n := GetService(t)
+func TestCreate(t *testing.T) {
+	n := GetService(t)
 
-    if n.DefinitionUrl != serviceUrl {
-        t.Fail()
-    }
+	if n.DefinitionUrl != serviceUrl {
+		t.Fail()
+	}
 }
 
-func TestGetResources(t *testing.T){
-    n := GetService(t)
+func TestGetResources(t *testing.T) {
+	n := GetService(t)
 
-    err := n.GetResources()
-    if err != nil {
-        t.Errorf("Couldn't get resources, %v", err)
-        t.Fail()
-    }
+	err := n.GetResources()
+	if err != nil {
+		t.Errorf("Couldn't get resources, %v", err)
+		t.Fail()
+	}
 
-    for _, r := range n.resources.Resources {
-        t.Log(r.Type)
-    }
+	for _, r := range n.resources.Resources {
+		t.Log(r.Type)
+	}
 }
 
-func TestGetPackage(t *testing.T){
-    n := GetService(t)
+func TestGetPackage(t *testing.T) {
+	n := GetService(t)
 
-    pkgs := []struct {
-        id string
-        fail bool
-    }{
-        { id: "Newtonsoft.Json", fail: true },
-        { id: "Sitecore.Kernel.NoReference", fail: false },
-    }
+	pkgs := []struct {
+		id   string
+		fail bool
+	}{
+		{id: "Newtonsoft.Json", fail: true},
+		{id: "Sitecore.Kernel.NoReference", fail: false},
+	}
 
-    for _, p := range pkgs {
-        t.Run("Package " + p.id, func (t *testing.T){
-            pkg, err := n.GetPackageData(p.id)
-            if err != nil && p.fail {
-                t.Log(err)
-                t.Fail()
-                return
-            }
+	for _, p := range pkgs {
+		t.Run("Package "+p.id, func(t *testing.T) {
+			pkg, err := n.GetPackageData(p.id)
+			if err != nil && p.fail {
+				t.Log(err)
+				t.Fail()
+				return
+			}
 
-            if len(pkg.Versions) == 0 {
-                t.Log("No versions")
-            }
-        })
-    }
+			if len(pkg.Versions) == 0 {
+				t.Log("No versions")
+			}
+		})
+	}
 }
 
-func TestDownloadPackage(t *testing.T){
-    n := GetService(t)
+func TestDownloadPackage(t *testing.T) {
+	n := GetService(t)
 
-    pkgs := []struct {
-        id string
-        version string
-    }{
-        { id: "Newtonsoft.Json", version: "8.0.1" },
-        { id: "Glass.Mapper.Sc" , version: "4.2.1.188" },
-        { id: "bootstrap" , version: "4.0.0" },
-    }
+	pkgs := []struct {
+		id      string
+		version string
+	}{
+		{id: "Newtonsoft.Json", version: "8.0.1"},
+		{id: "Glass.Mapper.Sc", version: "4.2.1.188"},
+		{id: "bootstrap", version: "4.0.0"},
+	}
 
-    for _, p := range pkgs {
-        t.Run("Package " + p.id + "-" + p.version, func (t *testing.T){
-            pkg, err := n.GetPackageData(p.id)
-            if err != nil {
-                t.Log(err)
-                t.Fail()
-                return
-            }
+	for _, p := range pkgs {
+		t.Run("Package "+p.id+"-"+p.version, func(t *testing.T) {
+			pkg, err := n.GetPackageData(p.id)
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+				return
+			}
 
-            v := n.GetVersion(pkg, p.version)
+			v := n.GetVersion(pkg, p.version)
 
-            s,err := n.DownloadVersion(v, ".")
+			s, err := n.DownloadVersion(v, ".")
 
-            if err != nil {
-                t.Log(err)
-                t.Fail()
-            }
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+			}
 
-            t.Log(s)
-        })
-    }
+			t.Log(s)
+		})
+	}
 }
 
-func TestDownloadAndExtractPackage(t *testing.T){
+func TestDownloadAndExtractPackage(t *testing.T) {
 
-    pkgs := []struct {
-        id string
-        version string
-    }{
-        { id: "Newtonsoft.Json", version: "8.0.1" },
-        { id: "Glass.Mapper.Sc" , version: "4.2.1.188" },
-        { id: "bootstrap" , version: "4.0.0" },
-    }
+	pkgs := []struct {
+		id      string
+		version string
+	}{
+		{id: "Newtonsoft.Json", version: "8.0.1"},
+		{id: "Glass.Mapper.Sc", version: "4.2.1.188"},
+		{id: "bootstrap", version: "4.0.0"},
+	}
 
-    for _, p := range pkgs {
-        t.Run("Package " + p.id + "-" + p.version, func (t *testing.T){
-            fn, err := DownloadAndExtract(serviceUrl, p.id, p.version, `c:\test\test`, `c:\test\extract`)
-            if err != nil {
-                t.Log(err)
-                t.Fail()
-                return
-            }
+	for _, p := range pkgs {
+		t.Run("Package "+p.id+"-"+p.version, func(t *testing.T) {
+			fn, err := DownloadAndExtract(serviceUrl, p.id, p.version, `c:\test\test`, `c:\test\extract`)
+			if err != nil {
+				t.Log(err)
+				t.Fail()
+				return
+			}
 
-            t.Log(fn)
-        })
-    }
+			t.Log(fn)
+		})
+	}
 }
 
-func TestGetLatestVersion(t *testing.T){
-    pkgs := []struct {
-        id string
-        version string
-        result bool // should equal or should not equal
-    }{
-        { id: "Newtonsoft.Json", version: "8.0.1", result: false },
-        { id: "Glass.Mapper.Sc" , version: "4.2.1.188", result: false },
-        { id: "bootstrap" , version: "4.0.0", result: true },
-    }
+func TestGetLatestVersion(t *testing.T) {
+	pkgs := []struct {
+		id      string
+		version string
+		result  bool // should equal or should not equal
+	}{
+		{id: "Newtonsoft.Json", version: "8.0.1", result: false},
+		{id: "Glass.Mapper.Sc", version: "4.2.1.188", result: false},
+		{id: "bootstrap", version: "4.0.0", result: true},
+		{id: "Microsoft.AspNet.Mvc", version: "5.2.4", result: true},
+	}
 
-        for _, p := range pkgs {
-        t.Run("Package " + p.id + "-" + p.version, func (t *testing.T){
-            vstr,err := GetLatestVersion(serviceUrl, p.id)
-            if err != nil {
-                t.Logf("Got error, expecting version, %v", err)
-                t.Fail()
-            }
+	for _, p := range pkgs {
+		t.Run("Package "+p.id+"-"+p.version, func(t *testing.T) {
+			vstr, err := GetLatestVersion(serviceUrl, p.id)
+			if err != nil {
+				t.Logf("Got error, expecting version, %v", err)
+				t.Fail()
+			}
 
-            if vstr != p.version && p.result {
-                t.Logf("Versions not equal. %v and got latest from service: %v", p.version, vstr)
-                t.Fail()
-            } else {
-                t.Logf("Got latest version from service for %v, latest is %v. The result is expected.", p.id, vstr)
-            }
-        })
-    }
+			if vstr != p.version && p.result {
+				t.Logf("Versions not equal. %v and got latest from service: %v", p.version, vstr)
+				t.Fail()
+			} else {
+				t.Logf("Got latest version from service for %v, latest is %v. The result is expected.", p.id, vstr)
+			}
+		})
+	}
 }
